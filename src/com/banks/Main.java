c3163ceba45931c3b3c3065125c306f576797c10
Bankole Salako <bsalako@seamfix.com>
2016-07-11 14:57:46 +0100

    Init2

--- src/com/banks/Main.java
+++ src/com/banks/Main.java
@@ -1,11 +1,10 @@
 package com.banks;
 
-import org.apache.commons.io.FileUtils;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(FileUtils.getTempDirectoryPath());
+//        System.out.println(FileUtils.getTempDirectoryPath());
     }
 }
